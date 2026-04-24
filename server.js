8f396705f5799071ee39fa938f2d03fccb8dbdf8
ROSHAN SHAIKH <roshaikh21@gmail.com>
2026-04-24 14:50:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -177,6 +177,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(4000, () => {
-  console.log("Server running: http://localhost:4000");
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
